bfe4c5907eeec99178e5b91171cadb6dfd8bf93e
Alireza Heydari <alireza97hi@gmail.com>
2017-12-01 02:39:37 +0330

    hill-climbing got fixed

--- hill-climbing.py
+++ hill-climbing.py
@@ -1,15 +1,17 @@
 class HillClimbing():
-    def __init__(self, problem,*args, **kwargs):
+    def __init__(self, problem, *args, **kwargs):
         self.problem = problem
-    
-    def solve(start, *args, **kwargs):
-        current_state = self.problem.start
+
+    def solve(self, *args, **kwargs):
+        current_state = self.problem.initial_state()
         best_found = False
         attempts = 1000
         while not best_found:
-            neighbour_nodes = self.problem.neighbours()
-            best_neighbour_node = min(neighbour_nodes,key=lambda x:self.problem.heuristic(x))
+            neighbour_nodes = self.problem.neighbours(current_state)
+            best_neighbour_node = min(
+                neighbour_nodes, key=lambda x: self.problem.heuristic(x))
             attempts -= 1
-            if is_goal(best_neighbour_node) or attemps < 0:
+            if self.problem.heuristic(best_neighbour_node) < self.problem.heuristic(current_state):
+                current_state = best_neighbour_node
+            if self.problem.is_goal(current_state) or attempts < 0:
                 best_found = True
-
