ab66ca4c73925bce6979b7743177569babda1579
Alireza Heydari <alireza97hi@gmail.com>
2017-12-02 01:49:20 +0330

    Simulated Annealing added

---
+++ SimulatedAnnealing.py
@@ -0,0 +1,25 @@
+import random
+from math import exp
+
+class SimulatedAnnealing():
+    def __init__(self, problem, *args, **kwargs):
+        self.problem = problem
+    
+    def solve(self):
+        current_state = self.problem.initial_state()
+        count = 0
+        local_try = 1000
+        temperature = 10000
+        sch = 0.99
+        t_min = 1
+        while temperature > t_min:
+            random_neighbour = random.choice(self.problem.neighbours(current_state))
+            efficiency = self.problem.heuristic(random_neighbour) - self.problem.heuristic(current_state)
+            #Replace new state if it is better than older one
+            if efficiency < 0:
+                current_state = random_neighbour
+            #Replace new state if the probability is good
+            elif exp((-(efficiency))/temperature) > random.random():
+                current_state = random_neighbour
+            temperature *= sch
+        print(current_state)
